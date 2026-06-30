0782757a589728d2a413937d42388efe5018c909
acierto-incomodo <97026099+acierto-incomodo@users.noreply.github.com>
2026-06-30 22:34:19 +0200

    Act 4

--- BuildWin.ps1
+++ BuildWin.ps1
@@ -1,3 +1,3 @@
 ./Clear.ps1
 python -m PyInstaller --onefile --windowed --noconsole --icon=meccha-chameleon.ico NoCompatibleToInstall.py
-echo v2.2.1 > GameVersion.txt
+echo "v2.2.1 ModsBuild v1" > GameVersion.txt
